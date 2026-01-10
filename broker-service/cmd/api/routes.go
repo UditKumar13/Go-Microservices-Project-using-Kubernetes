8b5bbbe5c9f6c43be83bfdf00cb65a776f7b609f
UditKumar13 <udit21148@gmail.com>
2026-01-10 17:54:09 +0530

    making the routes for the broker services

---
+++ broker-service/cmd/api/routes.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+)
+
+func routes() http.Handler {
+
+	mux := chi.NewRouter()
+
+	// specify who is allowed to access the resource
+
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+}
