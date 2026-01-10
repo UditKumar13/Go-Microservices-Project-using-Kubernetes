e5d7c19c13161f52a71c9b228c4474521fad3268
UditKumar13 <udit21148@gmail.com>
2026-01-10 21:38:28 +0530

    adding docker file for broker and docker-compose.yml

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,18 +1,23 @@
-import "net/http"
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
 
 type jsonResponse struct {
-	Erro    bool   `json:"error"`
+	Error   bool   `json:"error"`
 	Message string `json:"message"`
 	Data    any    `json:"data,omitempty"`
 }
 
-func (app *Config) Broker(w http.ResponseWritter, r *http.Request) {
+func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
 		Error:   false,
 		Message: "Hit the Broker",
 	}
 
-	out, _ = json.MarshalIndent(payload, "", "\t")
+	out, _ := json.MarshalIndent(payload, "", "\t")
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusAccepted)
 	w.Write(out)
